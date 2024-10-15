d8c27a01b218cad8473e898e2976e697357a29f3
dimitris <dimitris.androutsos@jdriven.com>
2024-10-15 15:36:45 +0200

    push code

---
+++ src/main/java/com/openrewrite/example/example/ExampleApplication.java
@@ -0,0 +1,12 @@
+package com.openrewrite.example.example;
+
+import org.springframework.util.ObjectUtils;
+
+public class ExampleApplication {
+
+	public static void main(String[] args) {
+		String s ="hello world";
+		ObjectUtils.isEmpty(s);
+	}
+
+}
